4981f8413184a3ee1fa3b9c9fef894aeeaa75e3e
swandrn <159421702+swandrn@users.noreply.github.com>
2024-08-27 01:27:34 +0200

    remove done print

--- cc/extract.py
+++ cc/extract.py
@@ -1,4 +1,3 @@
-from os import path
 from sys import exit as sysexit
 import pandas as pd
 
@@ -7,7 +6,6 @@ def read_csv(csv):
         print("Attempting to insert CSV into dataframe...")
         df = pd.read_csv(csv)
         print("CSV successfully inserted!")
-        print("Done")
         return df
     except Exception as e:
         sysexit(f"error reading the file: {e}")
