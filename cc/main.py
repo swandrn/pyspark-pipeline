7d2b2ea74244b3651c02fc3cafad93ba99b538f2
swandrn <159421702+swandrn@users.noreply.github.com>
2024-08-27 01:39:01 +0200

    implement ETL functions

---
+++ cc/main.py
@@ -0,0 +1,9 @@
+import paths
+import extract as e
+import transform as t
+import load as l
+
+df = e.read_csv(paths.RAW_CSV)
+df = t.drop_columns(df, "Unnamed: 0", "lat", "long", "city_pop", "merch_lat", "merch_long")
+l.df_to_sql(df)
+print("Done")
