0d9e46c19062c2b3a1d5178e3e07471f02cd3974
swandrn <159421702+swandrn@users.noreply.github.com>
2024-08-27 18:33:17 +0200

    handle case where no columns args are passed

--- cc/transform.py
+++ cc/transform.py
@@ -5,9 +5,12 @@ def drop_columns(df, *columns : str):
         if not isinstance(column, str):
             sysexit("drop_columns(*columns) only takes string type as argument")
     try:
-        print(f"Dropping columns {columns}...")
-        df = df.drop(columns=list(columns))
-        print("Successfully dropped columns!")
+        if not len(columns) == 0:
+            print(f"Dropping columns {columns}...")
+            df = df.drop(columns=list(columns))
+            print("Successfully dropped columns!")
+        else:
+            print("No columns to drop")
         return df
     except Exception as e:
         sysexit(f"error dropping columns: {e}")
