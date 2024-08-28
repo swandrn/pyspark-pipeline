9a6f81a1ebd9fa815c56e01537812f2290afb526
swandrn <159421702+swandrn@users.noreply.github.com>
2024-08-28 19:52:06 +0200

    move database creation to main

--- cc/load.py
+++ cc/load.py
@@ -1,15 +1,4 @@
 from sys import exit as sysexit
-from sqlalchemy import create_engine
-
-# Create database at root of project dir
-def create_db(db_name: str):
-    try:
-        print(f"Attempting to create SQLite database {db_name}...")
-        engine = create_engine(f"sqlite:///{db_name}.db")
-        print("SQLite database successfully created!")
-        return engine
-    except Exception as e:
-        sysexit(f"error creating the sqlite database {db_name}: {e}")
 
 # Create table and insert dataframe in sqlite
 def df_to_sql(df, engine):
--- cc/main.py
+++ cc/main.py
@@ -2,9 +2,14 @@ import paths
 import extract as e
 import transform as t
 import load as l
+from sqlalchemy import create_engine
 
 df = e.read_csv(paths.RAW_CSV)
 df = t.drop_columns(df, "Unnamed: 0", "lat", "long", "city_pop", "merch_lat", "merch_long")
-engine = l.create_db("credit_cards")
+
+db_name = "credit_cards"
+print(f"Attempting to create SQLite database {db_name}...")
+engine = create_engine(f"sqlite:///{db_name}.db")
+print("SQLite database successfully created!")
 l.df_to_sql(df, engine)
 print("Done")
