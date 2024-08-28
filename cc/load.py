fdea359fa36f7bb85ca53a444da58d931ff931d8
swandrn <159421702+swandrn@users.noreply.github.com>
2024-08-28 18:39:53 +0200

    move creation of database into its own function

--- cc/load.py
+++ cc/load.py
@@ -1,12 +1,19 @@
 from sys import exit as sysexit
 from sqlalchemy import create_engine
 
-#Create database, table, and insert dataframe
-def df_to_sql(df):
+# Create database at root of project dir
+def create_db(db_name: str):
     try:
-        print("Attempting to create SQLite database...")
-        engine = create_engine(f"sqlite:///credit_cards.db")
+        print(f"Attempting to create SQLite database {db_name}...")
+        engine = create_engine(f"sqlite:///{db_name}.db")
         print("SQLite database successfully created!")
+        return engine
+    except Exception as e:
+        sysexit(f"error creating the sqlite database {db_name}: {e}")
+
+# Create table and insert dataframe in sqlite
+def df_to_sql(df, engine):
+    try:
         print("Inserting dataframe into SQLite database...")
         df.to_sql(name='cc_records', con=engine.raw_connection(), if_exists='replace')
         print("Dataframe successfully inserted!")
