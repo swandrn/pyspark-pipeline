7209ef93f245d4181ffa7e0719cbf4793aec6822
swandrn <159421702+swandrn@users.noreply.github.com>
2024-08-28 18:43:26 +0200

    create database before inserting dataframe

--- cc/main.py
+++ cc/main.py
@@ -5,5 +5,6 @@ import load as l
 
 df = e.read_csv(paths.RAW_CSV)
 df = t.drop_columns(df, "Unnamed: 0", "lat", "long", "city_pop", "merch_lat", "merch_long")
-l.df_to_sql(df)
+engine = l.create_db("credit_cards")
+l.df_to_sql(df, engine)
 print("Done")
