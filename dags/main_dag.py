17d4c9ffbc5991903397aebc5a9dc42afb37495c
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-11 14:35:09 +0200

    run pyspark etl as a DAG

---
+++ dags/main_dag.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+
+spark_conf = {
+    'spark.jars.packages': 'org.apache.hadoop:hadoop-aws:3.3.6,org.apache.hadoop:hadoop-common:3.3.6,com.amazonaws:aws-java-sdk-bundle:1.12.367',
+}
+
+with DAG(
+    dag_id='PySpark-ETL-pipeline',
+) as dag:
+    
+    print_path = BashOperator(
+        task_id='print_path',
+        bash_command="""
+            airflow info;
+        """,
+    )
+
+    start_etl = EmptyOperator(
+        task_id='start_etl',
+    )
+    
+    etl_pipeline = SparkSubmitOperator(
+        task_id='etl_pipeline',
+        name='credit_cards',
+        application='./dags/main.py',
+        conn_id='spark_default',
+        conf=spark_conf,
+        total_executor_cores='1',
+        executor_cores='1',
+        executor_memory='2g',
+        num_executors='1',
+        driver_memory='2g',
+        verbose=False,
+    )
+
+    end_etl = EmptyOperator(
+        task_id='end_etl',
+    )
+
+print_path >> start_etl >> etl_pipeline >> end_etl
