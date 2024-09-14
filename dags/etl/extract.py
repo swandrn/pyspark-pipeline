ee8a8ab04ed1179fe5525fe0bfd43b4272d28edc
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-15 00:58:55 +0200

    multithreading support on call_random_user function

--- dags/etl/extract.py
+++ dags/etl/extract.py
@@ -1,6 +1,11 @@
 from sys import exit as sysexit
 from pyspark.sql import DataFrame, SparkSession
 import requests
+import threading
+from queue import Queue
+
+MAX_WORKERS = 5
+pool = threading.BoundedSemaphore(MAX_WORKERS)
 
 def read_csv(spark: SparkSession, csv: str) -> DataFrame:
     try:
@@ -13,17 +18,31 @@ def read_csv(spark: SparkSession, csv: str) -> DataFrame:
     except Exception as e:
         sysexit(f"error reading the file: {e}")
 
-def call_random_user(url: str, retry: bool = False):
+def call_random_user(url: str, q: Queue, retry: bool = False):
     try:
-        resp = requests.get(url)
-        if not resp.status_code == 200:
-            resp.raise_for_status()
-        user_json = resp.json()["results"][0]
-        return user_json
+        with pool:
+            resp = requests.get(url)
+            if not resp.status_code == 200:
+                resp.raise_for_status()
+            user_json = resp.json()["results"][0]
+            q.put(user_json)
     except requests.Timeout as e:
         if retry == True:
-            sysexit(f'error fetching {url}: {e}')
+            q.put(f'error fetching {url}: {e}')
         print('a timeout error occured, retrying...')
-        call_random_user(url=url, retry=True)
+        call_random_user(url=url, q=q, retry=True)
     except Exception as e:
-        sysexit(f'error fetching {url}: {e}')
+        q.put(f'error fetching {url}: {e}')
+
+# threads = []
+# users_json = []
+
+# for i in range(10):
+#     threads.append(threading.Thread(target=call_random_user, args=['https://randomuser.me/api/1.4/?exc=id']))
+
+# for thread in threads:
+#     thread.start()
+#     users_json.append(q.get())
+
+# for thread in threads:
+#     thread.join()
