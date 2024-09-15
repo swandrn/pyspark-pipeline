3f1cc8cd942b4c475d1c379347975fc745efa06b
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-15 22:21:44 +0200

    create function to check if key exists in s3 and fix imports

--- dags/etl/extract.py
+++ dags/etl/extract.py
@@ -1,8 +1,11 @@
 from sys import exit as sysexit
+import sparkenv
 from pyspark.sql import DataFrame, SparkSession
 import requests
 import threading
 from queue import Queue
+import boto3
+from botocore.exceptions import ClientError
 
 MAX_WORKERS = 5
 consumers = threading.BoundedSemaphore(MAX_WORKERS)
@@ -33,6 +36,22 @@ def call_random_user(url: str, q: Queue, retry: bool = False):
         call_random_user(url=url, q=q, retry=True)
     except Exception as e:
         q.put(f'error fetching {url}: {e}')
+
+def s3_key_exists(bucket: str, key: str) -> bool:
+    session = boto3.Session(aws_access_key_id=sparkenv.ACCESS_KEY_ID,
+                            aws_secret_access_key=sparkenv.SECRET_ACCESS_KEY,
+                            )
+    s3 = session.resource('s3',
+                        region_name='eu-north-1',
+                        )
+    try:
+        s3.Object(bucket, key).load()
+        return True
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False
+        else:
+            sysexit(f'error fetching {key} in {bucket}: {e}')
 
 # threads = []
 # users_json = []
