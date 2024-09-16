b527ed585e3a4899371e6034f61ba757d4b7fd57
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-16 13:58:39 +0200

    orchestrate etl from API to S3 bucket

---
+++ dags/usertos3_etl_dag.py
@@ -0,0 +1,78 @@
+from etl import extract
+from etl import transform
+from etl import load
+from etl import paths
+import threading
+from queue import Queue
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+
+def repeatedly_call_users(iter: int) -> list:
+    q = Queue()
+    threads = []
+    users_json = []
+    RESULTS = 50
+
+    for _ in range(iter):
+        threads.append(threading.Thread(target=extract.call_random_user, args=[f'https://randomuser.me/api/1.4/?exc=id&results={RESULTS}&nat=gb', q]))
+
+    for thread in threads:
+        thread.start()
+        users_json = users_json + q.get()
+
+    for thread in threads:
+        thread.join()
+
+    return users_json
+
+with DAG(
+    dag_id='Userme-S3-pipeline',
+    render_template_as_native_obj=True,
+) as dag:
+    
+    print_path = BashOperator(
+        task_id='print_path',
+        bash_command="""
+            airflow info;
+        """,
+    )
+
+    start_etl = EmptyOperator(
+        task_id='start_etl',
+    )
+
+    fetch_users = PythonOperator(
+        task_id='fetch_users',
+        provide_context=True,
+        do_xcom_push=True,
+        python_callable=repeatedly_call_users,
+        op_kwargs={
+            'iter': '100'
+        },
+    )
+
+    convert_to_df = PythonOperator(
+        task_id='convert_to_df',
+        provide_context=True,
+        do_xcom_push=True,
+        python_callable=transform.user_json_to_df,
+        op_args=["{{task_instance.xcom_pull(task_ids='fetch_users')}}"],
+    )
+
+    write_to_s3 = PythonOperator(
+        task_id='write_to_s3',
+        python_callable=load.pandas_df_to_s3,
+        op_kwargs={
+            'df': '{{task_instance.xcom_pull(task_ids="convert_to_df")}}',
+            'bucket': paths.CC_BUCKET,
+            'key': 'test_destination/users.csv',
+        },
+    )
+
+    end_etl = EmptyOperator(
+        task_id='end_etl',
+    )
+
+print_path >> start_etl >> fetch_users >> convert_to_df >> write_to_s3 >> end_etl
