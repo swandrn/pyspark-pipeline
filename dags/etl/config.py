dbaa8aadfa4ebf3b5ed7a12dd05d775c6ab44fb6
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-22 18:22:53 +0200

    move api call amount to confing and add how long workers should sleep as rate_limit

--- dags/etl/config.py
+++ dags/etl/config.py
@@ -1,5 +1,10 @@
+API_CALL = 50
+
 # User results per call
-RESULTS = 200
+RESULTS = 1000
 
 # Maximum amount of workers in a Bounded Semaphore
 MAX_WORKERS = 3
+
+# Time for a worker to sleep in second
+RATE_LIMIT = 0.5
