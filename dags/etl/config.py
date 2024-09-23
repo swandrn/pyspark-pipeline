b4d4a8f2f9b1d249a35039e3e222a27e5260bbe3
swandrn <159421702+swandrn@users.noreply.github.com>
2024-09-23 10:33:08 +0200

    update values

--- dags/etl/config.py
+++ dags/etl/config.py
@@ -1,10 +1,10 @@
-API_CALL = 50
+API_CALL = 20
 
 # User results per call
-RESULTS = 1000
+RESULTS = 5000
 
 # Maximum amount of workers in a Bounded Semaphore
-MAX_WORKERS = 3
+MAX_WORKERS = 1
 
 # Time for a worker to sleep in second
-RATE_LIMIT = 0.5
+RATE_LIMIT = 16
